162ea4e515a222b46b394b5fd422a34ed76c9c9c
Revolver Ocelot(Revolver Ocelot) <danpgaldino@gmail.com>
2019-12-02 20:17:18 -0300

    Update TimeLine.java

--- TrabalhoAndroid/app/src/main/java/com/trab/myapplication/Activity/TimeLine.java
+++ TrabalhoAndroid/app/src/main/java/com/trab/myapplication/Activity/TimeLine.java
@@ -127,7 +127,7 @@ public class TimeLine extends AppCompatActivity {
                 e.printStackTrace();
             }
 
-            return null;
+            return false;
         }
 
         @Override
